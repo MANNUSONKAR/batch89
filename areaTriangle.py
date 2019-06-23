f53524ed457b32eaaa083580a5c9c665f6661cca
User <mannusonkarteach123@gmail.com>
2019-06-22 21:51:23 -0700

    mannu added area of circle

--- areaTriangle.py
+++ areaTriangle.py
@@ -7,3 +7,9 @@ c = float(input("Enter third side of Triangle : "))
 s = a+b+c
 area = sqrt(s*(s-a)*(s-b)*(s-c))
 print("Area of Triangle = ", area)
+# area of circle
+
+r = float(input("Enter the radius of circle : "))
+arcy = 3.14*r*r
+print("area of circle=",arcy)
+
