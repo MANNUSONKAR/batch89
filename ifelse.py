ef57e86f98e8b0ee7a86b9049d4ae522de55c27c
User <mannusonkarteach123@gmail.com>
2019-07-03 22:14:13 -0700

    user and password

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+a = 5
+if a == 5:
+    print("yes its equel")
+
+usere = "karan"
+
+password = "mannu"
+
+if usere == "karan" and password == "mannu":
+    print("welcome   " + usere)
+else:
+    print("please check user or password")
+
+
